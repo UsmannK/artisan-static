55f58fb547ef99ebd1f3c7b55b8f33370a4cf497
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-15 11:27:24 +0800

    Delete source directory

--- source/_partials/head/favicon.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- favicon -->
-{{-- try out https://realfavicongenerator.net --}}
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<!-- end favicon -->
--- source/_posts/favicons.md
+++
@@ -1,7 +0,0 @@
----
-title: '02 - Favicons'
-date: 2018-08-31
-image: icons.jpg
-comments: false
----
-Recommendation: [RealFaviconGenerator](https://realfavicongenerator.net)
